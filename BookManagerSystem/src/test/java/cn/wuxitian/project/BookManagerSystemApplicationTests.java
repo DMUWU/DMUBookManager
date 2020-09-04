132bfbed646a4cb5c4975406936305fde00cb4c8
wuxit <wuxit@DESKTOP-2V24A3G>
2020-09-04 13:48:19 +0800

    start from springboot

---
+++ BookManagerSystem/src/test/java/cn/wuxitian/project/BookManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wuxitian.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
