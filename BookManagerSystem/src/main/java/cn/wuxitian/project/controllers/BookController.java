85a155dc167c2bc8c678a794df5ab2747a0bf453
wuxitian <wuxitian@DESKTOP-2V24A3G>
2020-09-04 20:57:48 +0800

    test hello

---
+++ BookManagerSystem/src/main/java/cn/wuxitian/project/controllers/BookController.java
@@ -0,0 +1,21 @@
+package cn.wuxitian.project.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+	@RequestMapping("/go3")
+	public String toIndex() {
+
+		return "go1";
+		
+	}
+	
+	
+}
